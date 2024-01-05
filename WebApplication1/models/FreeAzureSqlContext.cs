5243b255a05376e1ecc6369632c1e904b6d40725
Mariya Kabdulova <105773495+marionum@users.noreply.github.com>
2024-01-05 18:10:15 +0200

    Update FreeAzureSqlContext.cs

--- WebApplication1/models/FreeAzureSqlContext.cs
+++ WebApplication1/models/FreeAzureSqlContext.cs
@@ -26,7 +26,7 @@ public partial class FreeAzureSqlContext : DbContext
 #pragma warning disable CS1030 // #warning directive
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
 
-        optionsBuilder.UseSqlServer("Server=tcp:koodaajakoulutus.database.windows.net,1433;Initial Catalog=FreeAzureSQL;Persist Security Info=False;User ID=paavo;Password=Str#ng_Passw#rd;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        optionsBuilder.UseSqlServer("Server=tcp:serverforwebapi.database.windows.net,1433;Initial Catalog=FreeAzureSQL;Persist Security Info=False;User ID=s2300294@edu.bc.fi@serverforwebapi;Password=a1300807M;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 #pragma warning restore CS1030 // #warning directive
         
     }
